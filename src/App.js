6347efd17c16d62adeda9ff7fe8c4fef2b94200c
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-09-30 15:41:02 +0300

    class based react components study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Navbar } from "./components/Navbar";
+import { TodoRows } from "./components/TodoRows";
+export default class App extends Component {
+  //-----------------constructor here-------------------------------
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    this.state = {
+      userName: "David",
+      todoItems: [
+        { action: "Buy milk", done: true },
+        { action: "Dentist at 5 pm", done: false },
+        { action: "Go to Gym", done: false }
+      ],
+      newToDo: "",
+    };
+  }
+  // ----------------functions here-----------------------------
+
+  toggleDone = (todo) => {
+    return this.setState({
+      todoItems: this.state.todoItems.map((item) => {
+        return item.action === todo.action ? { ...item, done: !item.done } : item
+      }),
+    })
+  }
+
+
+
+  updateValue = (event) => {
+    return this.setState({ newToDo: event.target.value });
+  }
+
+  newToDo = () => {
+    this.setState({
+      todoItems: [
+        ...this.state.todoItems,
+        { action: this.state.newToDo, done: false }
+      ]
+    })
+  }
+
+  todoRows = () => {
+    return this.state.todoItems.map((item) => 
+    <TodoRows key={item.action} item={item} callback={this.toggleDone} /> );
+  }
+
+  // ------------------------------JSX here-------------------------------
+  render = () => (
+    <div className="container">
+      <div className="row">
+        <Navbar name={this.state.userName} />
+        <div className="col-12">
+          <input
+            className="form-control"
+            value={this.state.newToDo}
+            onChange={this.updateValue}
+          />
+          <button
+            className="btn btn-primary m-2"
+            onClick={this.newToDo}
+          >
+            Add
+          </button>
+        </div>
+        <div className="col-12">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Complete</th>
+              </tr>
+            </thead>
+            <tbody>{this.todoRows()}</tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
-  );
+
+  )
+
 }
 
-export default App;
+
+
---
+++ src/components/TodoRows.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export class TodoRows extends Component {
+  render = () =>
+  (
+      <tr>
+        <td>{this.props.item.action}</td>
+        <td>
+          <input
+          type="checkbox"  
+          checked={this.props.item.done}
+          role="button"
+          onChange={() => this.props.callback(this.props.item)}
+          />
+        </td>
+      </tr>
+    )
+}
+
